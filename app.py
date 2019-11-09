394829b92f7dd0af5569170840d307be8a4f4b70
Takayoshi <a>
2019-11-09 15:09:15 +0900

    処理をmain関数に委譲

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
 import requests
 
-# postal_code = input('郵便番号を入力してください(ハイフンなし7桁) ＞')
-postal_code = '0287302'
 
+def main():
+    global postal_code
+    # postal_code = input('郵便番号を入力してください(ハイフンなし7桁) ＞')
+    postal_code = '0287302'
 
-def search_address():
-    global address
-    response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}')
-    results = response.json()['results'][0]
-    都道府県名 = results['address1']
-    市区町村名 = results['address2']
-    町域名 = results['address3']
-    address = f'{都道府県名}{市区町村名}{町域名}'
+    def search_address():
+        global address
+        response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}')
+        results = response.json()['results'][0]
+        都道府県名 = results['address1']
+        市区町村名 = results['address2']
+        町域名 = results['address3']
+        address = f'{都道府県名}{市区町村名}{町域名}'
+        print(address)
 
-
-search_address()
-
-print(address)
+if __name__ == '__main__':
+    main()
