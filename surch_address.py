d05f4d18d2502dd32508f9466b5bc1d48bf88c97
Takayoshi <a>
2019-11-09 17:52:35 +0900

    addressserch_testを実装した

--- surch_address.py
+++ surch_address.py
@@ -1,11 +1,19 @@
 import requests
 
 
-def search_address(postal_code):
-    response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}')
+def search_address(zipcode):
+    response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={zipcode}')
     results = response.json()['results'][0]
     都道府県名 = results['address1']
     市区町村名 = results['address2']
     町域名 = results['address3']
     address = f'{都道府県名}{市区町村名}{町域名}'
     return address
+
+
+if __name__ == '__main__':
+    zipcode = '0287111'
+
+    address = search_address(zipcode)
+
+    assert '岩手県八幡平市大更' == address
