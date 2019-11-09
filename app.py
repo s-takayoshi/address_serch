fb227253ff03269b2ae01f841359b1bfad67f5bb
Takayoshi <a>
2019-11-09 14:26:58 +0900

    住所1,2,3を出力する機能を追加

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import requests
 
-postal_code = input('郵便番号を入力してください(ハイフンなし7桁) ＞')
+# postal_code = input('郵便番号を入力してください(ハイフンなし7桁) ＞')
+postal_code = '0287302'
 
 response = requests.get(f'http://zipcloud.ibsnet.co.jp/api/search?zipcode={postal_code}')
+results = response.json()['results'][0]
 
-print(response)
-print(response.text)
+都道府県名 = results['address1']
+市区町村名 = results['address2']
+町域名 = results['address3']
 
+address = f'{都道府県名}{市区町村名}{町域名}'
+print(address)
+
+# print(response.json())
